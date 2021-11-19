d459d59116915aa97be7b1a8c0684e2bec4e905b
John <john@digitalscrub.com>
2021-11-19 12:08:39 +0000

    Change Bastion Name

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -1,13 +1,13 @@
 # Copyright © 2020, Oracle and/or its affiliates. 
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-resource "oci_bastion_bastion" "demoBastion" {
+resource "oci_bastion_bastion" "Bastion" {
   bastion_type = "STANDARD"
   client_cidr_block_allow_list = [
     "0.0.0.0/0",
   ]
   compartment_id = var.compartment_ocid
   max_session_ttl_in_seconds = "10800"
-  name                       = "demoBastion"
+  name                       = "Bastion"
   target_subnet_id = oci_core_subnet.subnet_private.id
 }

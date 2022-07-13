e32e4018174a0d61590fbe03484294117b693daa
John Lathouwers <john.lathouwers@oracle.com>
2022-07-13 15:16:28 +0100

    Put back, secret now quoted

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,6 +5,7 @@ terraform {
     bucket                      = "terraform-backend"
     key                         = "terraform.tfstate"
     skip_region_validation      = true
+    skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
   }
